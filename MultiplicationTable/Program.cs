6f8a74099044fa39e91490fdcd268716d0ce54b4
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 13:19:40 +0300

    Initial commit

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,15 @@
+﻿class MultiplicationTable
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double result = 0;
+
+        for (int i = 1; i <=10; i++) 
+        { 
+            result = n * i;
+            Console.WriteLine($"{n} x {i} = {result}");
+            
+        }
+    }
+}
